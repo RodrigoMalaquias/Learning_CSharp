b5705b1a846545b5dd90d8d49cf6f2d04d8e2796
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2020-10-29 16:11:31 -0300

    Seção 2

---
+++ Classes, atributos, métodos, membros estáticos/Primeiros-Exercicios (classe)/Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario A, B;
+            A = new Funcionario();
+            B = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            A.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            A.salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            B.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            B.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double SalarioMedio;
+
+            SalarioMedio = (A.salario + B.salario) / 2.0;
+
+            Console.WriteLine("Salário médio = "+SalarioMedio.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
