1bd60d1b4b8d0ea0aa4fca42d49308f7c66c1de6
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2020-10-29 16:16:00 -0300

    Seção 2

---
+++ Classes, atributos, métodos, membros estáticos/Exercicio de Fixação/Exercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            x.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Área: "+x.Area().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro: "+x.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: "+x.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        
+        }
+    }
+}
---
+++ Classes, atributos, métodos, membros estáticos/Exercicio de Fixação/Exercicio 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            double porcentagem;
+
+            Console.Write("Nome: ");
+            f.nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: "+ f);
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            f.AumentarSalario(double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados atualizados: "+f);
+
+        }
+    }
+}
