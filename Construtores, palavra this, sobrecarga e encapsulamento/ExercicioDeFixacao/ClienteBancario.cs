36e7239a2ed153ea3e0b684c78819ee1268331ac
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2020-10-29 16:17:46 -0300

    Seção 3

---
+++ Construtores, palavra this, sobrecarga e encapsulamento/ExercicioDeFixacao/ClienteBancario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExercicioDeFixacao
+{
+    class ClienteBancario
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public ClienteBancario(int numeroConta, string nome)
+        {
+            NumeroConta = numeroConta;
+            Nome = nome;
+            Saldo = 0; // Não é preciso, apenas enfatizei.
+        }
+        public ClienteBancario(int numeroConta, string nome, double depositoInicial): this(numeroConta, nome)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Saque (double valor)
+        {
+            Saldo = Saldo - valor -5;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "+NumeroConta+", Titular: "+Nome+", Saldo: $ " + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
