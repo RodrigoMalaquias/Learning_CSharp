3169588668429ca8e539574d80fc36e87370b728
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2020-10-29 16:26:12 -0300

    Seção 6

---
+++ Comportamento de memória, arrays, listas/ExercicioDeFixacaoLista/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioDeFixacaoLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Console.Write("How many employees will be registred? ");
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i<=n; i++)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: ");
+                int Id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string Name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double Salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                list.Add(new Employee(Id, Name, Salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int IdRequested = int.Parse(Console.ReadLine());
+            Employee emp = list.Find(x => x.Id == IdRequested);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double Percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.increaseSalary(Percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+}
+    }
+}
---
+++ Comportamento de memória, arrays, listas/ExercicioDeFixacaoVetores/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExercicioDeFixacaoVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] vect = new Student[10];
+
+            int n = int.Parse(Console.ReadLine());
+            int cont = 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Rent #"+cont+":");
+                cont++;
+                Console.Write("Name: "); 
+                string Name = Console.ReadLine();
+                Console.Write("Email: ");
+                string Email = Console.ReadLine();
+                Console.Write("Room: ");
+                int Room = int.Parse(Console.ReadLine());
+                vect[Room] = new Student { name = Name, email = Email , room = Room};
+            }
+
+            Console.WriteLine("Busy Rooms: ");
+            for(int i = 0; i<10; i++)
+            {
+                if(vect[i] != null)
+                    Console.WriteLine(i+": "+vect[i].name+", "+vect[i].email);
+
+            }
+        }
+    }
+}
